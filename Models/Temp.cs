687f7dd0492715cf98d3d9cf760f5ac809584469
tengrui <tengrui13@gmail.com>
2020-02-15 18:38:06 -0500

    20200215 Final

---
+++ Models/Temp.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieMaven.Models
+{
+    public static class Temp
+    {
+        public static string searchTerm { get; set; }
+    }
+}
