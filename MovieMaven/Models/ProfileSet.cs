d774d36a07789ae43b92caaa9eab1a0b0a7d120d
Trui-git <34838847+Trui-git@users.noreply.github.com>
2020-02-22 08:23:20 -0500

    add more missing files

---
+++ MovieMaven/Models/ProfileSet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieMaven.Models
+{
+    public class ProfileSet
+    {
+        public List<Profile> profiles { get; set; }
+        public int id { get; set; }
+    }
+}
---
+++ MovieMaven/Models/RelatedSet.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieMaven.Models
+{
+    public class RelatedSet
+    {
+        public int page { get; set; }
+        public int total_results { get; set; }
+        public int total_pages { get; set; }
+        public List<Related> results { get; set; }
+    }
+}
