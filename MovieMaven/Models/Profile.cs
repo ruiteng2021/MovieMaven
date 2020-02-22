d67b125471c9bc4b5fd95f1904f1b559f0430e88
Trui-git <34838847+Trui-git@users.noreply.github.com>
2020-02-22 08:17:51 -0500

    add missing file

---
+++ MovieMaven/Models/Profile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MovieMaven.Models
+{
+    public class Profile
+    {
+        public object iso_639_1 { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public int vote_count { get; set; }
+        public double vote_average { get; set; }
+        public string file_path { get; set; }
+        public double aspect_ratio { get; set; }
+    }
+}
